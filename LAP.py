f3dc6d4260ec932263544bff0ead7fa295b7d57e
Apaala Chatterjee <apaala@gmail.com>
2020-07-30 12:56:18 -0400

    adding functionality and testing prep locally

--- LAP.py
+++ LAP.py
@@ -1,12 +1,15 @@
 import docker
 import getopt, sys, os 
 from optparse import OptionParser 
+import tabix
 
 parser = OptionParser() 
 parser.add_option("-t", "--target", dest="target",help="Path to target", metavar="FILE") 
 parser.add_option("-r", "--reference", dest="reference",help="Path to reference", metavar="FILE") 
 parser.add_option("-n", "--name", dest="name",help="Name for output file", metavar="NAME")
 parser.add_option("-o", "--out", dest="outdir",help="output dir", metavar="Out")
+parser.add_option("-c", "--chr", dest="chrom",help="chromosome", metavar="NUM") 
+parser.add_option("-s", "--sample", dest="samples",help="samplelist", metavar="FILE")
 (options, args) = parser.parse_args() 
 
 client = docker.from_env()
@@ -35,21 +38,46 @@ def prep_mod1(target, fname):
 def prep_mod_local(target,ref, fname, outdir):
     bgcmd="bgzip -c "+target+"> "+outdir+"/"+fname+"_"+os.path.basename(target)+".gz" 
     bgzipf=outdir+"/"+fname+"_"+os.path.basename(target)+".gz"
+    print(bgcmd)
 #    os.system(cmd)
+
     tabxcmd="tabix -p vcf "+bgzipf
+    print(tabxcmd)
 #    os.system(tabxcmd)
     tbxf=bgzipf+".tbi"
     bcfcmd="bcftools filter --include 'AN=2*N_SAMPLES' -Oz -o "+outdir+"/"+fname+ "_out.vcf.gz "+ bgzipf
-#    os.system(bcfcmd)
-    bcffile=outdir+"/"+fname+ "_out.vcf.gz
-    conformcmd="java -jar /usr/local/packages/conform-gt/conform-gt.jar ref="+ref+" gt="+bcffile+"chrom=1"+" out=conform"+fname
-    print(conformcmd)
-    print(bgcmd)
-    print(bgzipf)
-    print(tabxcmd)
     print(bcfcmd)
+#    os.system(bcfcmd)
+    bcffile=outdir+"/"+fname+ "_out.vcf.gz"
+    conformcmd="java -jar /usr/local/packages/conform-gt/conform-gt.jar ref="+ref+" gt="+bcffile+" chrom=1"+" out=conform"+fname
+    print(conformcmd)
+    conformf=outdir+ "/conform" +fname 
+    conformtbx="tabix "+conformf
+    
+    print(conformf)
+#    $bcftools merge -m none -o $temp -Oz --threads 2 $conformed_file $peru_1KG
+    bcfm="bcftools merge -m none -o mergeout_"+ fname+".vcf.gz -Oz --threads 2 "+ conformf+ " "+ref
+    #$bcftools view -h $vcf | tail -1 | tr '\t' '\n' | grep "NWD" > temp.chr$chr.txt
+    print(bcfm)
+    mfile="mergeout_"+ fname+".vcf.gz"
+    bcfc="bcftools view -h "+mfile+" | tail -1 | tr '\t' '\n' | grep 'NWD' > temp_"+mfile
+    print(bcfc)
+    #cat temp.chr$chr.txt /home/dloesch/WORKSPACE/rfmix.ref_samples.txt > samples.chr$chr.txt 
+    #ccmd="cat temp_"+mfile+ 
+    #$bcftools view -S samples.chr$chr.txt -e 'GT[*] = "mis"' -o $merged -Oz $temp 
+    #ref=/local/chib/toconnor_grp/LARGE-PD/1KG/HG38/chr$chr.1kg.HG38.vcf.gz 
+    #java -jar $conform ref=$ref gt=$merged chrom=chr$chr out=$prefix.merged.chr$chr.CONFORM match=POS 
+    #tabix $prefix.merged.chr$chr.CONFORM.vcf.gz 
+    #print(bgcmd)
+    #print(bgzipf)
+    #print(tabxcmd)
+    #print(bcfcmd)
 
-prep_mod_local(options.target, options.name, options.outdir) 
+
+#def beagle_rfmix(target, ref, fname):
+    
+
+prep_mod_local(options.target,options.reference, options.name, options.outdir) 
 
 
 #client.containers.run("apaala/beagle:0.1", "sh /home/beagle.example")
